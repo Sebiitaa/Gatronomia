d910c5eb5831d35ce45e2f96f9c3bf30719cd3be
Sebiitaa <seb.garay@duocuc.cl>
2024-08-15 17:29:15 -0400

    update

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 // Tu URL de Supabase y la clave pública
 const supabaseUrl = 'https://ukuvffbluwfmoqxbjrms.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrdXZmZmJsdXdmbW9xeGJqcm1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM3NTIzNjAsImV4cCI6MjAzOTMyODM2MH0.FNARtf3lSBZ0kdgg0zwGgoXxYjTaf9yPrLPQP1opjAo'
-
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export { supabase }
