c5d2112b81963611e0fcea0f1677e50a3caee0a3
Sebiitaa <seb.garay@duocuc.cl>
2024-08-15 15:41:06 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,22 @@
-// Importa las funciones necesarias del SDK de Firebase
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
-// Configuración de Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA93i8ti9ToScMxrYEhrfEs-LCGlg5PadM",
   authDomain: "base-de-datos-7c044.firebaseapp.com",
   databaseURL: "https://base-de-datos-7c044-default-rtdb.firebaseio.com",
   projectId: "base-de-datos-7c044",
-  storageBucket: "gs://base-de-datos-7c044.appspot.com
-",
+  storageBucket: "base-de-datos-7c044.appspot.com",
   messagingSenderId: "492201117643",
   appId: "1:492201117643:web:5306795b35be34bc70984e",
   measurementId: "G-GGL68K97V1"
 };
 
-// Inicializa Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
